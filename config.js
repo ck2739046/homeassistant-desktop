9dc1755926650d74412ef91380a475ef8b87660f
DustyArmstrong <pseudokitten@gmail.com>
2025-01-05 09:04:19 +0000

    Default shortcut update

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const store = new Store({
     fullScreen: false,
     shortcutEnabled: true,
     shortcutFullscreenEnabled: false,
-    userShortcut: "CommandOrControl+Shift+X",
+    userShortcut: "CommandOrControl+Alt+X",
     allInstances: [],
   },
 });
